087ccdb575c073a9142495dd28b377c4db9a5081
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-11-16 20:12:03 +0530

    added ansible connection

--- 40-db/bootstrap.sh
+++ 40-db/bootstrap.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 sudo dnf install ansible -y
+
 ansible-pull \
   -U https://github.com/gangadharkamanaboyina/expenseApp-ansible.git \
   main.yaml \
-  -i inventory.ini \
-  --inventory-file=inventory.ini \
-  --limit localhost \
+  --limit "localhost" \
   -e "component=mysql"
-
