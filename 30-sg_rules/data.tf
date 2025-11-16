76e8272527e97385e3854a65289dd457bfd63f45
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-11-16 15:35:18 +0530

    changed sg rules

--- 30-sg_rules/data.tf
+++ 30-sg_rules/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "backend_lb_sg_id" {
 data "aws_ssm_parameter" "bastion_sg_id" {
   name = "/${var.project}/${var.env}/bastion_sg_id"
 }
+
+data "aws_ssm_parameter" "mysql_sg_id" {
+  name = "/${var.project}/${var.env}/mysql_sg_id"
+}
--- 30-sg_rules/locals.tf
+++ 30-sg_rules/locals.tf
@@ -1,4 +1,5 @@
 locals {
     backend_lb_sg_id = data.aws_ssm_parameter.backend_lb_sg_id.value
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
 }
