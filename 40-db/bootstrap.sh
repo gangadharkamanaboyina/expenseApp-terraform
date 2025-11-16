176ab8c8b73e41ac574d8759bcc900e7f266efcf
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-11-16 17:04:47 +0530

    added user_data

--- 40-db/bootstrap.sh
+++ 40-db/bootstrap.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 sudo dnf install ansible -y
-ansible-pull -U https://github.com/gangadharkamanaboyina/expenseApp-ansible.git -e component=mysql main.yaml
+ansible-pull \
+  -U https://github.com/gangadharkamanaboyina/expenseApp-ansible.git \
+  main.yaml \
+  -i inventory \
+  -e "component=mysql"
