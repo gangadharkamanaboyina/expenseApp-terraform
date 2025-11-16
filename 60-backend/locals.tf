71f6da63f2a9bd69b31b70fb13add696517e3c23
Gangadhar <gangadharkamanaboyina@gmail.com>
2025-11-16 21:08:30 +0530

    added ansible connection

--- 60-backend/locals.tf
+++ 60-backend/locals.tf
@@ -3,5 +3,5 @@ locals {
     ami_id = data.aws_ami.DevopsPrac.id
     instance_type = "t3.micro"
     sg_id = data.aws_ssm_parameter.backend_sg_id.value
-    db_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+    private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 }
